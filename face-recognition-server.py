1a53ff6e37f995b2bb722365ced061c01d300fb0
El Goumri Reda <redabusiness10@gmail.com>
2024-12-05 17:25:28 +0100

    10000 port

--- face-recognition-server.py
+++ face-recognition-server.py
@@ -134,7 +134,7 @@ def compare_faces(unknown_face_embedding):
 
 # Main server
 async def main():
-    server = await websockets.serve(websocket_handler, "0.0.0.0", 10000)
+    server = await websockets.serve(websocket_handler, "0.0.0.0", 8765)
     print("Server started at ws://0.0.0.0")
     await server.wait_closed()
 
